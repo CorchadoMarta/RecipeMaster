26b83dc7332b6ba5e81734e8236808f118e2c36f
Marta <gcatram@gmail.com>
2016-01-13 18:00:56 +0100

    all

--- RecipeMaster/src/KitchenDao.java
+++ RecipeMaster/src/KitchenDao.java
@@ -6,6 +6,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.SortedMap;
+
+import com.mysql.jdbc.MysqlErrorNumbers;
 
 public class KitchenDao {
 	private Connection connect = null;
@@ -90,8 +96,14 @@ public class KitchenDao {
 
 			statement.executeUpdate(sentence);
 
-		} catch (SQLException | ClassNotFoundException e) {
-			System.out.println(e.toString());
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + name);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -111,8 +123,14 @@ public class KitchenDao {
 					+ description + "')";
 			statement.executeUpdate(sentencia);
 
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + description);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -138,8 +156,14 @@ public class KitchenDao {
 			// Result set get the result of the SQL query
 			ps.executeUpdate();
 			
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + description);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -159,8 +183,14 @@ public class KitchenDao {
 			String sentencia = "INSERT INTO allergen (id_Allergen, name) values (" + id_Allergen + ", '" + name + "')";
 			statement.executeUpdate(sentencia);
 
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + name);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -180,8 +210,14 @@ public class KitchenDao {
 					+ ", '" + id_Allergen + "')";
 			statement.executeUpdate(sentencia);
 
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + code_ingredient + " " +  id_Allergen);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -210,8 +246,14 @@ public class KitchenDao {
 			ps.setString(8, free);
 			ps.executeUpdate();
 
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + step);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -236,8 +278,14 @@ public class KitchenDao {
 			// Result set get the result of the SQL query
 			ps.executeUpdate();
 
-		} catch (SQLException | ClassNotFoundException e) {
-			throw e;
+		} catch (SQLException e) {
+			if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+				System.out.println("Duplicate entry: " + description);
+			} else {
+				System.out.println(e.getErrorCode());
+				System.out.println(e.getMessage());
+				throw e;
+			}
 		} finally {
 			close();
 		}
@@ -274,9 +322,9 @@ public class KitchenDao {
 		
 	}
 	
-	public ArrayList<Ingredient> getIngredients(int codRecipe) {
+	public HashMap<String, Double> getIngredients(int codRecipe) {
 		//declarations
-		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+		HashMap<String, Double> ingredients = new HashMap<String, Double>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			// Setup the connection with the DB
@@ -286,32 +334,139 @@ public class KitchenDao {
 			// Statements allow to issue SQL queries to the database
 			statement = connect.createStatement();
 			// Result set get the result of the SQL query
-			resultSet = statement.executeQuery("select a.id_allergen, a.name from allergen a inner join has_allergen h on a.id_allergen=h.id_allergen INNER JOIN ingredient i on h.code_ing=i.code inner join procedure_recipe p on i.code=p.code_ingredient where p.code_recipe=" + codRecipe);
+			resultSet = statement.executeQuery("select i.name, p.quantity from ingredient i inner join procedure_recipe p on i.code=p.code_ingredient where p.code_recipe=" + codRecipe +  " and name!='NA';");
 			while (resultSet.next()) { 
-				int id = resultSet.getInt("id_allergen");
-				String name = resultSet.getString("name");
-				//ingredients.add(new Ingredient(id, name));
+				//ingredients.put(name,);
+				ingredients.put(resultSet.getString("name"), resultSet.getDouble("p.quantity"));
 			}
 			return ingredients;
-			
 		} catch (Exception e) {
 			
+		} finally {
+			close();
 		}
 
 		return ingredients;
 		
 	}
 	
+	public HashMap<String, Double> getNutritionalValues(int codRecipe) {
+		//declarations
+		HashMap<String, Double> nutritionalValues = new HashMap<String, Double>();
+		/* double kCalPerson = 0;
+		double carbohydratesPerson = 0;
+		double proteinPerson = 0;
+		double */
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager
+					.getConnection("jdbc:mysql://localhost/kitchen?" + "user=root&password=jupiter");
+
+			// Statements allow to issue SQL queries to the database
+			statement = connect.createStatement();
+			// Result set get the result of the SQL query
+			String sSQL = "select sum(i.kCal)/r.serves as kCalPerson, sum(i.carbohydrates)/r.serves as carbohydratesPerson, sum(i.protein)/r.serves as proteinPerson, ";
+			sSQL += " sum(i.fat)/r.serves as fatPerson, sum(i.salt)/r.serves as saltPerson from ingredient i inner join procedure_recipe p on i.code=p.code_ingredient ";
+			sSQL += " inner join ref_recipe r on r.code_recipe=p.code_recipe where p.code_recipe=" + codRecipe + " and name!='NA';";
+			resultSet = statement.executeQuery(sSQL);
+			while (resultSet.next()) { 
+				nutritionalValues.put("kCal", resultSet.getDouble("kCalPerson"));
+				nutritionalValues.put("carbohydrates", resultSet.getDouble("carbohydratesPerson"));
+				nutritionalValues.put("protein", resultSet.getDouble("proteinPerson"));
+				nutritionalValues.put("fat", resultSet.getDouble("fatPerson"));
+				nutritionalValues.put("salt", resultSet.getDouble("saltPerson"));
+			}
+			return nutritionalValues;
+		} catch (Exception e) {
+			
+		} finally {
+			close();
+		}
+
+		return nutritionalValues;
+		
+	}
+	
+	public ArrayList<String> getProcedureList(int codRecipe) throws Exception {
+		//declarations
+		ArrayList<String> procedure = new ArrayList<String>();
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager
+					.getConnection("jdbc:mysql://localhost/kitchen?" + "user=root&password=jupiter");
+
+			// Statements allow to issue SQL queries to the database
+			statement = connect.createStatement();
+			// Result set get the result of the SQL query
+			resultSet = statement.executeQuery("select p.step, c.description, i.name, p.time, p.temperature, p.free from procedure_recipe p inner join cooking_procedure c on p.code_procedure = c.code inner join ingredient i on p.code_ingredient = i.code where p.code_recipe = " + codRecipe + ";");
+			while (resultSet.next()) { 
+				int step = resultSet.getInt("p.step");
+				String verb = resultSet.getString("c.description");
+				String ingredient = resultSet.getString("i.name"); 
+				int time = resultSet.getInt("p.time");
+				String timeSolved = "";
+				if (time > 0) {
+					int timeSec = time % 60;
+					int timeMin = (time / 60) % 60;
+					int timeHour = time / 3600;
+					timeSolved = timeHour + "h, " + timeMin + "m, " + timeSec + "s."; 
+				}
+
+				double temperature = resultSet.getDouble("p.temperature");
+				String free = resultSet.getString("p.free");
+				procedure.add(step + "- " + verb + " " + ingredient + " " + timeSolved + " " + temperature + " " + free + ".");
+			}
+			
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			close();
+		}
+		return procedure;
+	}
+	
 	public String getRecipeText(int codRecipe) throws Exception {
 		
 		//declarations
 		String fullRecipe = "";
+		String name = "";
 		try {
-			
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager
+					.getConnection("jdbc:mysql://localhost/kitchen?" + "user=root&password=jupiter");
+
+			// Statements allow to issue SQL queries to the database
+			statement = connect.createStatement();
+			// Result set get the result of the SQL query
+			resultSet = statement.executeQuery("select description from ref_recipe where code_recipe= " + codRecipe);
+			while (resultSet.next()) { 
+				name = resultSet.getString("description") + "\n";
+			}
+			//title (name) of the recipe
+			name = Character.toString(name.charAt(0)).toUpperCase() + name.substring(1);
+			fullRecipe += name + "\n";
+			//ingredients
+			fullRecipe += "Ingredients\n";
+			HashMap<String, Double> ingreQuant = getIngredients(codRecipe);
+			System.out.println(ingreQuant.size());
+			for (Map.Entry<String, Double> entry : ingreQuant.entrySet()) {
+				fullRecipe += entry.getKey() + ": " +  entry.getValue() + "\n";
+			    //Object value = entry.getValue();
+			    // ...
+			}
+			ArrayList<String> procedures = getProcedureList(codRecipe);
+			for (String s : procedures) {
+				fullRecipe += s +"\n";
+			}
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			close();
 		}
-		
 		return fullRecipe;
 		
 	}
